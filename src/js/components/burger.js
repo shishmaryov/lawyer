746e50be3e3e6582085b3915d47aefcb27b34f00
Konstantin Shishmaryov <shishmaryovkonstantin@gmail.com>
2023-06-06 20:10:09 +0300

    header

---
+++ src/js/components/burger.js
@@ -0,0 +1,44 @@
+import { disableScroll } from '../functions/disable-scroll';
+import { enableScroll } from '../functions/enable-scroll';
+
+(function(){
+  const burger = document?.querySelector('[data-burger]');
+  const menu = document?.querySelector('[data-menu]');
+  const menuItems = document?.querySelectorAll('[data-menu-item]');
+  const overlay = document?.querySelector('[data-menu-overlay]');
+
+  burger?.addEventListener('click', (e) => {
+    burger?.classList.toggle('burger--active');
+    menu?.classList.toggle('mobile-menu--active');
+		overlay?.classList.toggle('overlay--active');
+
+    if (menu?.classList.contains('mobile-menu--active')) {
+      burger?.setAttribute('aria-expanded', 'true');
+      burger?.setAttribute('aria-label', 'Закрыть меню');
+      disableScroll();
+    } else {
+      burger?.setAttribute('aria-expanded', 'false');
+      burger?.setAttribute('aria-label', 'Открыть меню');
+      enableScroll();
+    }
+  });
+
+  overlay?.addEventListener('click', () => {
+    burger?.setAttribute('aria-expanded', 'false');
+    burger?.setAttribute('aria-label', 'Открыть меню');
+    burger.classList.remove('burger--active');
+		overlay.classList.remove('overlay--active');
+    menu.classList.remove('mobile-menu--active');
+    enableScroll();
+  });
+
+  menuItems?.forEach(el => {
+    el.addEventListener('click', () => {
+      burger?.setAttribute('aria-expanded', 'false');
+      burger?.setAttribute('aria-label', 'Открыть меню');
+      burger.classList.remove('burger--active');
+      menu.classList.remove('mobile-menu--active');
+      enableScroll();
+    });
+  });
+})();

34008857fbe2fe92612b6c2821b9c7df8cdd91d6
Konstantin Shishmaryov <shishmaryovkonstantin@gmail.com>
2023-06-08 20:14:59 +0300

    services

--- src/js/components/swiper.js
+++ src/js/components/swiper.js
@@ -2,7 +2,7 @@ import Swiper, {Navigation, Pagination, Scrollbar} from 'swiper';
 
 Swiper.use([Navigation, Pagination, Scrollbar]);
 
-let swiper = new Swiper('.swiper', {
+let heroSlider = new Swiper('.hero__swiper', {
 	slidesPerView: 1,
 	loop: true,
 
@@ -18,6 +18,25 @@ let swiper = new Swiper('.swiper', {
 			},
     }
   }
-	
+
 
 });
+
+const mediaQuery = window.matchMedia('(max-width: 768px)');
+if (mediaQuery.matches) {
+  document.querySelector('.services__cards').classList.add('swiper');
+  document.querySelector('.services__wrapper').classList.add('swiper-wrapper');
+  let s = document.querySelectorAll('.service');
+  let index = 0;
+
+  for (index = 0; index < s.length; ++index) {
+    s[index].classList.add('swiper-slide');
+  }
+
+  let servicesSlider = new Swiper('.services__cards', {
+  slidesPerView: 1.5,
+	spaceBetween: 24,
+  loop: false,
+
+});
+}
